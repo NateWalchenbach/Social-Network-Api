77965303025da4d66399343f2a234c4a73ba917c
NateWalchenbach <natewalchenbach24@gmail.com>
2022-03-25 09:06:47 -0700

    added DB connection methods

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then(() => console.log('DB connection successful'));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
