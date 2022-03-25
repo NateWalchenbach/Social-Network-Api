84cea14b12578dd43008216995ac1e6bbca4d1f0
Nate Walchenbach <natewalchenbach24@gmail.com>
2022-03-24 23:35:45 -0700

    created thought model

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, 'A thought must have text'],
+      minlength: [1, 'Please enter your thought'],
+      maxlength: [280, 'Please have thoughts less than 280 characters...'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => date.toLocaleString(),
+    },
+    username: {
+      type: Schema.Types.String,
+      ref: 'User',
+      required: [true, 'A thought must be linked to a username...'],
+      validate: {
+        validator: function (username) {
+          return User.exists({ username });
+        },
+        message: 'User does not exist',
+      },
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema.post('save', function (thought, next) {
+  User.findOneAndUpdate(
+    { username: thought.username },
+    { $addToSet: { thoughts: thought } },
+    { select: '-username' },
+    next()
+  );
+});
+
+// Friends virtual
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions && this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
